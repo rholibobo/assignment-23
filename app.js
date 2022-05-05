9178cfec82df5809f3dda0898d344052d7a3b034
rholibobo <rholibobo@gmail.com>
2022-05-05 11:52:37 +0100

    assignment 23rd

---
+++ app.js
@@ -0,0 +1,20 @@
+// Question 1
+
+let arr = [];
+ let array1 = [20, 46, 90, 80, 120];
+ let array2 = [2, 2, 5, 4, 2];
+
+var array3 = array1[0] / array2[0];
+var array4 = array1[1] / array2[1];
+var array5 = array1[2] / array2[2];
+var array6 = array1[3] / array2[3];
+var array7 = array1[4] / array2[4];
+
+
+arr.push(array3);
+arr.push(array4);
+arr.push(array5);
+arr.push(array6);
+arr.push(array7);
+
+console.log(arr);
